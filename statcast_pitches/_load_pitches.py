91f6a20f5df66059c49e1c00e53c2da00a8282cf
Jensen Holm <holmj@mail.gvsu.edu>
2024-10-30 10:07:35 -0400

    package working, needed to register 'pitches' db.

--- statcast_pitches/_load_pitches.py
+++ statcast_pitches/_load_pitches.py
@@ -4,7 +4,7 @@ import polars as pl
 import pandas as pd
 import duckdb
 
-from ._utils import HF_DATASET_LOC
+from ._utils import *
 
 __all__ = ["load"]
 
@@ -21,7 +21,7 @@ def _load_all_pandas() -> pd.DataFrame:
     return pd.read_parquet(HF_DATASET_LOC)
 
 
-def load(query: Optional[str] = None, pandas: bool = False) -> DataLike:
+def load(query: Optional[str], pandas: bool = False) -> DataLike:
     """
     Returns a DataFrame object (either polars or pandas) containing statcast pitch data
 
@@ -32,13 +32,28 @@ def load(query: Optional[str] = None, pandas: bool = False) -> DataLike:
         False to get a polars DataFrame.
 
     query : Optional[str]
-        optional duckdb SQL query to execute before loading data. This is suggested
-        when you don't want to download all 7M+ rows (575mb). query is None by default,
-        meaning if it is not specified you will download all of the pitches.
+        optional duckdb SQL query to execute before loading data. The name of the table is
+        registered as 'pitches'. This is suggested when you don't want to download all
+        7M+ rows (575mb). query is None by default, meaning if it is not specified you will
+        download all of the pitches.
+
+        example :
+            import statcast_pitches
+
+            query = f'''
+                SELECT bat_speed, swing_length
+                FROM pitches
+                WHERE YEAR(game_date) = '2024'
+                    AND bat_speed IS NOT NULL;
+                '''
+
+            swing_data_24_df = statcast_pitches.load(query=query)
     """
     if query is None:
         return _load_all_polars() if not pandas else _load_all_pandas()
 
-    with duckdb.connect(database=HF_DATASET_LOC) as con:
+    with duckdb.connect() as con:
+        _ = con.execute(INSTALL_DB_REQS_QUERY)
+        _ = con.execute(REGISTER_QUERY)
         result = con.sql(query=query)
         return result.pl() if not pandas else result.df()
