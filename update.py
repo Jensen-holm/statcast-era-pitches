01b2a02b162236e930a4aad22ecf4488fd62db10
Jensen Holm <jholm@cardinals.com>
2025-03-19 16:46:58 -0400

    update.py more robust & load_pitches * import removed

--- update.py
+++ update.py
@@ -1,7 +1,9 @@
 from huggingface_hub import HfApi
+from enum import Enum
 import polars as pl
 import pybaseball
 import datetime
+import logging
 import os
 
 from update.utils import LOCAL_STATCAST_DATA_LOC, HF_DATASET_LOC
@@ -17,36 +19,18 @@ warnings.filterwarnings("ignore")
 HF_TOKEN = os.environ.get("HF_TOKEN")
 
 
+class UpdateFlag(Enum):
+    COMPLETE = 0
+    NOT_NEEDED = 1
+    ERROR = 2
+
+
 def yesterday() -> datetime.date:
     return datetime.datetime.now().date() - datetime.timedelta(days=1)
 
 
-def refresh_statcast() -> int:
-    """Completley refreshed the hugging face dataset"""
-    _ = (
-        pl.from_pandas(
-            pybaseball.statcast(
-                start_dt="2015-04-05",
-                end_dt=yesterday().isoformat(),
-            ),
-            schema_overrides=STATCAST_SCHEMA,
-        )
-        # .with_columns(pl.col("game_date").cast(pl.Datetime("us")).alias("game_date"))
-        .with_columns(
-            pl.col("game_date").cast(pl.Date).alias("game_date")
-        ).write_parquet(LOCAL_STATCAST_DATA_LOC)
-    )
-
-    if not os.path.exists(LOCAL_STATCAST_DATA_LOC):
-        return 1
-    return 0
-
-
-def update_statcast(date: datetime.date, refresh: bool = False) -> int:
+def update_statcast(date: datetime.date) -> UpdateFlag:
     """Updates the statcast DataFrame with data from last date, to the date argument"""
-    if refresh:
-        return refresh_statcast()
-
     old_df = pl.scan_parquet(HF_DATASET_LOC)
     latest_date = (
         old_df.select("game_date")
@@ -58,22 +42,29 @@ def update_statcast(date: datetime.date, refresh: bool = False) -> int:
     )
 
     if latest_date == date or date.month in {12, 1, 2}:
-        print(f"No updates needed for {date}")
-        return 1
+        logging.log(logging.INFO, f"no updates needed for {date}")
+        return UpdateFlag.NOT_NEEDED
 
-    new_df = pl.from_pandas(
-        pybaseball.statcast(
-            start_dt=latest_date.strftime("%Y-%m-%d"),
-            end_dt=date.strftime("%Y-%m-%d"),
-        ),
-        schema_overrides=STATCAST_SCHEMA,
-    ).with_columns(pl.col("game_date").cast(pl.Date).alias("game_date"))
+    try:
+        new_df = pl.from_pandas(
+            pybaseball.statcast(
+                start_dt=latest_date.strftime("%Y-%m-%d"),
+                end_dt=date.strftime("%Y-%m-%d"),
+            ),
+            schema_overrides=STATCAST_SCHEMA,
+        ).with_columns(pl.col("game_date").cast(pl.Date).alias("game_date"))
 
-    updated_df = pl.concat([old_df.collect(), new_df], how="diagonal_relaxed")
-    updated_df.write_parquet(LOCAL_STATCAST_DATA_LOC)
+        updated_df = pl.concat([old_df.collect(), new_df], how="diagonal_relaxed")
+        updated_df.write_parquet(LOCAL_STATCAST_DATA_LOC)
+    except Exception as e:
+        logging.log(
+            logging.INFO,
+            f"dataset update failed with the exception {e.__class__}:\n\t{e}",
+        )
+        return UpdateFlag.ERROR
 
-    print(f"Saved New Statcast Data from {latest_date} to {date}")
-    return 0
+    logging.log(logging.INFO, f"saved new statcast data from {latest_date} to {date}")
+    return UpdateFlag.COMPLETE
 
 
 def upload_to_hf() -> None:
