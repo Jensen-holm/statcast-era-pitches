702089370a913b36eb8eee5c036fd314ca828d9d
Jensen Holm <jholm@cardinals.com>
2025-06-15 21:40:10 -0500

    adding ability to interpret code from .sql files

--- statcast_pitches/_utils.py
+++ statcast_pitches/_utils.py
@@ -1,3 +1,8 @@
+from typing import Optional
+import os
+
+__all__ = ["HF_DATASET_LOC", "INSTALL_DB_REQS_QUERY", "REGISTER_QUERY", "read_sql"]
+
 HF_DATASET_LOC = (
     "hf://datasets/Jensen-holm/statcast-era-pitches/data/statcast_era_pitches.parquet"
 )
@@ -13,3 +18,29 @@ REGISTER_QUERY = f"""
     CREATE VIEW pitches AS 
     SELECT * FROM parquet_scan('{HF_DATASET_LOC}');
 """
+
+
+def _is_sql(fp: str) -> bool:
+    ext = os.path.splitext(fp)[-1]
+    return True if os.path.exists(fp) and ext == ".sql" else False
+
+
+def read_sql(fp: str) -> Optional[str]:
+    """
+    Reads the contents of a SQL file if the input is a valid .sql file, None otherwise
+
+    Arguments
+    --------
+    fp : str
+        string or a filepath to a .sql file
+
+    Returns
+    -------
+    file contents of the file located at `fp` if it is a valid .sql file, else None
+    """
+    if not _is_sql(fp):
+        return None
+
+    with open(fp, "r") as f:
+        sql = f.read()
+    return sql
